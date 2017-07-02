9d135c05d4a5e9830d3aa291d6f8aaef2547bb12
Marianus <MMarianus@users.noreply.github.com>
2017-07-01 23:13:56 -0300

    Add files via upload

--- eureka.py
+++ eureka.py
@@ -6,32 +6,48 @@ import re
 import subprocess
 import argparse
 import os
+from collections import OrderedDict
 
-BANNER = '\nEureka! :)\n'
-USAGE = 'python eureka.py -f fileName.ext --ma --mails --urls --lang eng'
-DESCRIPTION = """
-Eureka! is search tool that identifies mail artifacts, urls, email addresses and human language in files (mostly, in memory dumps). NOTE: To save the results into a file, please redirect the output..
-"""
-STRINGS_FILE = "stringsOut.txt"
+def GetBanner():
+	return '\nEureka! :)\n'
+
+def GetUsage():
+	return 'python eureka.py -f fileName.ext --je --mails --urls --lang eng'
+
+def GetDescription():
+	return """
+	Eureka! is search tool that identifies mail artifacts, urls, email addresses and human language in files (mostly, in memory dumps). NOTE: To save the results into a file, please redirect the output..
+	"""
+
+def notFound():
+	return '\t[i] - No results found, but keep trying!.'
+
+def GetLangLetters(lang):
+	if 'esp' in lang:
+		return 'abcdefghijklmnopqrstuvwxqyzñáóé \t\n'
+	return 'abcdefghijklmnopqrstuvwxqyz \t\n'
+
+def GetOutFileName():
+	return "stringsOut.txt"
 
 def StringSearch(filename):
 	cmd = 'strings'
 	if os.name == 'nt':
 		cmd = 'strings2'
-	cmdStringSearch = cmd + ' ' + filename + ' > ' + STRINGS_FILE 
+	cmdStringSearch = cmd + ' ' + filename + ' > ' + GetOutFileName() 
 	subprocess.call(cmdStringSearch, shell=True)
 	
 def WarmingUp():
-	parser = argparse.ArgumentParser(description=DESCRIPTION, usage=USAGE)
+	parser = argparse.ArgumentParser(description=GetDescription(), usage=GetUsage())
 	parser.add_argument("-f", metavar='File_Name', nargs=1, required=True, help="File to analyze.")
-	parser.add_argument("--ma",action='store_true', help="Will search Mails. (Raw output)")
+	parser.add_argument("--je",action='store_true', help="Will search JSON Emails. (Raw output)")
 	parser.add_argument("--mails", action='store_true', help="Will search mail addresses.")
 	parser.add_argument("--urls", action='store_true', help="Will search URLs.")
 	parser.add_argument("--lang", metavar='\'eng\' [or] \'esp\'', nargs=1, help="Identify human language in a given file. 'esp' for spanish, 'eng' for english language identification. Example: python eureka.py --lan eng pagefile.sys")
 	
 	return parser.parse_args()
 
-def SearchArtefacts(fileName):
+def SearchMails(fileName):
 	with open(fileName,'r') as memDumpFile:
 		regex = r".*email\\.*"
 		re.compile(regex)
@@ -42,11 +58,7 @@ def SearchArtefacts(fileName):
 				found += 1
 				print match.group() 
 		if found == 0:
-			print('\t[i] - No Email artefacts were found buddy, but keep trying!.')
-
-EN_LETTERS = 'abcdefhijklmnopqrstuvwxqyz \t\n'
-ES_LETTERS = 'abcdefhijklmnopqrstuvwxqyzñáóé \t\n'
-DICT = ''
+			print(notFound())
 
 def SearchData(fileName, regex):
 	allData = {}
@@ -57,9 +69,9 @@ def SearchData(fileName, regex):
 			matches = re.finditer(regex, line)
 			for match in matches:
 				found += 1
-				allData[match.group().replace('u003c','')] = 'data'
+				allData[match.group().replace('u003c','').replace('u003e','').replace('u003d','')] = 'data'
 	if found == 0:
-		print('\t[i] - No results found, but keep trying!.')
+		print(notFound())
 	else:
 		for data in allData:
 			print data
@@ -73,80 +85,85 @@ def LoadDictionary(lang):
 	for word in dictionaryFile.read().split('\n'):
 		wordList[word] = 'found'
 	dictionaryFile.close()
-	global DICT
-	DICT = wordList
-	#return wordList
+	return wordList
 
-def GetWordCount(message, lang):
+def GetWordCount(message, lang, dictionary):
 	message = message.lower()
 	message = RemoveNonLetters(message, lang)
 	possibleWords = message.split()
-
-	if possibleWords == []:
+	
+	if possibleWords == [] or len(possibleWords)==1:
 		return 0.0 
-
-	matches = 0
-	for word in possibleWords:
-		if word in DICT:
-			matches += 1
+	
+	if depShorts(possibleWords):
+		matches = 0
+		for word in possibleWords:
+			if word in dictionary:
+				matches += 1
+	else:
+		return 0.0
 	return float(matches) / len(possibleWords)
+
+def depShorts(possibleWords):
+	cantSingle = 0
+	for word in possibleWords:
+		if len(word) < 2:
+			cantSingle += 1
+	return cantSingle <= len(possibleWords)/2
 
 def RemoveNonLetters(message, lang):
 	lettersOnly = []
-	if 'eng' in lang:
-		letters = EN_LETTERS
-	else:
-		letters = ES_LETTERS
 	for symbol in message:
-		if symbol in letters:
+		if symbol in GetLangLetters(lang):
 			lettersOnly.append(symbol)
 	return ''.join(lettersOnly)
 
-def IsValidLanguage(lang, message, wordPercentage=20, letterPercentage=80):
+def IsValidLanguage(lang, message, dictionary, wordPercentage=20, letterPercentage=80):
 	message = message.lower()
-	wordsMatch = GetWordCount(message, lang) * 100 >= wordPercentage
+	wordsMatch = GetWordCount(message, lang, dictionary) * 100 >= wordPercentage
 	numLetters = len(RemoveNonLetters(message, lang))
 	messageLettersPercentage = float(numLetters) / len(message) * 100
 	lettersMatch = messageLettersPercentage >= letterPercentage
 	return wordsMatch and lettersMatch
 
 def SearchLanguage(fileName, lang):
+	print("\n[i]- Now, Human Language is being searched..")
 	if not 'esp' in lang and not 'eng' in lang:
-		print('\t[!]- No valid language was provided.')
+		print("\t[!]- No valid language was provided.")
 	else:
-		LoadDictionary(lang)
+		print("\t[i]- Using %s language for detection." % ('English' if 'eng' in args.lang else 'Spanish'))
+		dictionary = LoadDictionary(lang)		
 		with open(fileName,'r') as file:
 			for line in file:
-				if IsValidLanguage(lang, line):
+				if IsValidLanguage(lang, line, dictionary):
 					print line.replace('\n','')
 				
 if __name__ == "__main__":
-	print BANNER
+	print(GetBanner())
 	args = WarmingUp()
 
-	if not args.ma and not args.mails and not args.lang and not args.urls:
-		print('\n[!]- Maybe you need help, as no parameter were provided. You can try with \'eureka.py -h\'..\n')
+	if not args.je and not args.mails and not args.lang and not args.urls:
+		print("\n[!]- Maybe you need help, as no parameter were provided. You can try with \'eureka.py -h\'..\n")
 	else:
-		print('\n[i]- Searching strings in the file, please be patient..')
+		print("\n[i]- Searching strings in the file, please be patient..")
 		StringSearch(args.f[0])
 
-	if args.ma:
-		print('\n[i]- Now, Email Artefacts are being searched..')
-		SearchArtefacts(STRINGS_FILE)
+	if args.je:
+		print("\n[i]- Now, JSON Emails are being searched..")
+		SearchMails(GetOutFileName())
 		
 	if args.mails:
-		print('\n[i]- Now, Email Addresses are being searched..')
-		mailReg = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
-		SearchData(STRINGS_FILE, mailReg)
+		print("\n[i]- Now, Email Addresses are being searched..")
+		mailReg = r"(?:[a-z0-9_-]+(?:\.[a-z0-9_-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+		SearchData(GetOutFileName(), mailReg)
 		
 	if args.urls:
-		print('\n[i]- Now, URLs are being searched..')
+		print("\n[i]- Now, URLs are being searched..")
 		urlReg = r"(?=https?://|ftp://|www\.)([^\s\"\'\)\>]+)"
-		SearchData(STRINGS_FILE, urlReg)
+		SearchData(GetOutFileName(), urlReg)
 		
 	if args.lang:
-		print('\n[i]- Now, Human Language is being searched..')
-		SearchLanguage(STRINGS_FILE, args.lang)
+		SearchLanguage(GetOutFileName(), args.lang)
 	
-	print('\n')
-	os.remove(STRINGS_FILE)
+	print("\n")
+	os.remove(GetOutFileName())
