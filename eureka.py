406e98a925646331eb9c9eedf78cbcf144a957f7
Marianus <MMarianus@users.noreply.github.com>
2019-04-24 15:56:01 +0200

    Update eureka.py
    - Fixed bugs in spanish language detection
    - Regex tweaks

--- eureka.py
+++ eureka.py
@@ -13,46 +13,46 @@ import argparse
 import os
 
 def GetBanner():
-	return "\nEureka! :)\n"
+    return "\nEureka! :)\n"
 
 def GetUsage():
-	return "python eureka.py -f fileName.ext --fb --je --mails --urls --lang eng"
+    return "python eureka.py -f fileName.ext --fb --je --mails --urls --lang eng"
 
 def GetDescription():
-	return """
-	Eureka! is search tool that identifies Facebook Chats, Emails, URLs, Email Addresses and Human Language in very, very large files (mostly, in memory dumps). NOTE: To save the results into a file, please redirect the output..
-	"""
+    return """
+    Eureka! is search tool that identifies Facebook Chats, Emails, URLs, Email Addresses and Human Language in very, very large files (mostly, in memory dumps). NOTE: To save the results into a file, please redirect the output..
+    """
 
 def NotFound():
-	return "\t[i] - No results found, but keep trying!."
+    return "\t[i] - No results found, but keep trying!."
 
 def GetLangLetters(lang):
-	if "esp" in lang:
-		return "abcdefghijklmnopqrstuvwxqyzñáóé \t\n"
-	return "abcdefghijklmnopqrstuvwxqyz \t\n"
+    if "esp" in lang:
+        return "abcdefghijklmnopqrstuvwxqyzñáóé \t\n"
+    return "abcdefghijklmnopqrstuvwxqyz \t\n"
 
 def LangDelTable(lang):
     return "".join(l.lower() for l in [chr(x) for x in range(256)] if l.lower() not in GetLangLetters(lang))
 
 def GetOutFileName():
-	return "stringsOut.txt"
+    return "stringsOut.txt"
 
 def StringSearch(filename):
-	cmd = "strings"
-	if os.name == "nt":
-		cmd = "strings2"
-	cmdStringSearch = cmd + " " + filename + " > " + GetOutFileName() 
-	subprocess.call(cmdStringSearch, shell=True)
-	
+    cmd = "strings"
+    if os.name == "nt":
+        cmd = "strings2"
+    cmdStringSearch = cmd + " " + filename + " > " + GetOutFileName() 
+    subprocess.call(cmdStringSearch, shell=True)
+    
 def WarmingUp():
-	parser = argparse.ArgumentParser(description=GetDescription(), usage=GetUsage())
-	parser.add_argument("-f", metavar='File_Name', nargs=1, required=True, help="File to analyze.")
-	parser.add_argument("--je",action='store_true', help="JSON Emails Search. (Raw output)")
-	parser.add_argument("--mails", action='store_true', help="Mail Addresses search.")
-	parser.add_argument("--urls", action='store_true', help="URLs search. For each detected URL, web vulnerabilities presence will be audited.")
-	parser.add_argument("--fb", action='store_true', help="Facebook chats search.")
-	parser.add_argument("--lang", metavar='\'eng\' [or] \'esp\'', nargs=1, help="Identify human language in a given file. 'esp' for spanish, 'eng' for english language identification. Example: python eureka.py -f pagefile.sys --lan eng ")
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description=GetDescription(), usage=GetUsage())
+    parser.add_argument("-f", metavar='File_Name', nargs=1, required=True, help="File to analyze.")
+    parser.add_argument("--je",action='store_true', help="JSON Emails Search. (Raw output)")
+    parser.add_argument("--mails", action='store_true', help="Mail Addresses search.")
+    parser.add_argument("--urls", action='store_true', help="URLs search. For each detected URL, web vulnerabilities presence will be audited.")
+    parser.add_argument("--fb", action='store_true', help="Facebook chats search.")
+    parser.add_argument("--lang", metavar='\'eng\' [or] \'esp\'', nargs=1, help="Identify human language in a given file. 'esp' for spanish, 'eng' for english language identification. Example: python eureka.py -f pagefile.sys --lan eng ")
+    return parser.parse_args()
 
 #ToDo: deprecate this functionality
 def SearchJSONMails(fileName):
@@ -67,11 +67,11 @@ def SearchJSONMails(fileName):
          print(NotFound())
             
 def FilterFBChat(chat):
-	filReg = "\"(?:thread_id|message_id|author|timestamp|body)\":\"{0,1}[^\"]+[\"|\,]"
-	re.compile(filReg)
-	matches2 = re.finditer(filReg, chat)
-	for match2 in matches2:
-		print(match2.group())
+    filReg = "\"(?:thread_id|message_id|author|timestamp|body)\":\"{0,1}[^\"]+[\"|\,]"
+    re.compile(filReg)
+    matches2 = re.finditer(filReg, chat)
+    for match2 in matches2:
+        print(match2.group())
 
 def SearchFBChat(fileName):
    print("\n[i]- Now, Facebook chats are being searched..")
@@ -109,42 +109,42 @@ def ValidateLine(line, regex):
     for match in matches:
         #return line.replace('u003c','').replace('u003e','').replace('u003d','').replace('\n','')
         return match.group().replace('u003c','').replace('u003e','').replace('u003d','')
-          		
+                  
 def LoadDictionary(lang):
-	if 'eng' in lang:
-		dictionaryFile = open('engDict.txt','r')
-	else:
-		dictionaryFile = open('espDict.txt','r')
-	wordList = {}
-	for word in dictionaryFile.read().split('\n'):
-		wordList[word] = 'found'
-	dictionaryFile.close()
-	return wordList
+    if 'eng' in lang:
+        dictionaryFile = open('engDict.txt','r')
+    else:
+        dictionaryFile = open('espDict.txt','r')
+    wordList = {}
+    for word in dictionaryFile.read().split('\n'):
+        wordList[word] = 'found'
+    dictionaryFile.close()
+    return wordList
 
 def GetWordCount(message, lang, dictionary):
-	possibleWords = message.split()
-	
-	if possibleWords == [] or len(possibleWords)==1:
-		return 0.0 
-	
-	if DepShorts(possibleWords):
-		matches = 0
-		for word in possibleWords:
-			if word in dictionary:
-				matches += 1
-	else:
-		return 0.0
-	return matches / len(possibleWords)
+    possibleWords = message.split()
+    
+    if possibleWords == [] or len(possibleWords)==1:
+        return 0.0 
+    
+    if DepShorts(possibleWords):
+        matches = 0
+        for word in possibleWords:
+            if word in dictionary:
+                matches += 1
+    else:
+        return 0.0
+    return matches / len(possibleWords)
 
 def DepShorts(possibleWords):
-	cantSingle = 0
-	for word in possibleWords:
-		if len(word) < 2:
-			cantSingle += 1
-	return cantSingle <= len(possibleWords)/2			
+    cantSingle = 0
+    for word in possibleWords:
+        if len(word) < 2:
+            cantSingle += 1
+    return cantSingle <= len(possibleWords)/2            
 
 def RemoveNonLetters(message, lang):
-    delTable = LangDelTable(lang[0])
+    delTable = LangDelTable(lang)
     translator = message.maketrans("","",delTable)
     return message.translate(translator)
 
@@ -159,7 +159,7 @@ def IsValidLanguage(lang, message, dictionary, wordPercentage=.2, letterPercenta
     
 def SearchUrls(fileName):
     print("\n[i]- Now, URLs are being searched..")
-    urlReg = r"(?=https?:\/\/|www\.|ftp:\/\/)([^\s\"\'\)\>]+)"
+    urlReg = r"(?=https?:\/\/|www\.|ftp:\/\/)([^\n\r]+)"
     urlList = SearchData(GetOutFileName(), urlReg)
     if urlList != NotFound():
         print("[i]- URLs were found:")
@@ -170,7 +170,7 @@ def SearchUrls(fileName):
         print(NotFound())
 
 def SearchXSS(url):
-    rdo = ValidateLine(url, r"((\%3C))((\%2F)|\/)*([a-z0-9\%]|\s)+(\/|((\%3E)))+|((\%22)(\%2f))")
+    rdo = ValidateLine(url, r"((\%3C)|(\<))((\%2F)|\/)*([a-z0-9\%]|\s)+(\/|((\%3E)|(\>))+|((\%22)((\%2f))))")
     if rdo:
         return "XSS"
     return ""
@@ -188,7 +188,7 @@ def SearchWebVulns(urlList):
     return urlList
     
 def SearchSQLi(url):
-    rdo = ValidateLine(url, r"(\%27\+)")
+    rdo = ValidateLine(url, r"(((\%27)|(\')|(\%027))((\+)|(\s)))")
     if rdo:
         return "SQLi"
     return ""
@@ -197,16 +197,16 @@ def WebVulnPrefix(vulnName):
     return "[Web Vuln Found: %s]" % (vulnName)
 
 def SearchLanguage(fileName, lang):
-	print("\n[i]- Now, Human Language is being searched..")
-	if not "esp" in lang and not "eng" in lang:
-		print("\t[!]- No valid language was provided.")
-	else:
-		print("\t[i]- Using %s language for detection." % ('English' if 'eng' in args.lang else 'Spanish'))
-		dictionary = LoadDictionary(lang)		
-		with open(fileName,'r') as file:
-			for line in file:
-				if IsValidLanguage(lang, line, dictionary):
-					print(line.replace('\n',''))
+    print("\n[i]- Now, Human Language is being searched..")
+    if not "esp" in lang and not "eng" in lang:
+        print("\t[!]- No valid language was provided.")
+    else:
+        print("\t[i]- Using %s language for detection." % ('English' if 'eng' in args.lang else 'Spanish'))
+        dictionary = LoadDictionary(lang)
+        with open(fileName,'r') as file:
+            for line in file:
+                if IsValidLanguage(lang, line, dictionary):
+                    print(line.replace('\n',''))
 
 def isURLValidation(regex):
    if "http" in regex:
@@ -223,7 +223,7 @@ def SearchEmails(fileName):
             print(mail)
     else:
         print(NotFound())
-			
+            
 if __name__ == "__main__":
     print(GetBanner())
     args = WarmingUp()
@@ -237,18 +237,18 @@ if __name__ == "__main__":
     
     if args.je:
         SearchJSONMails(GetOutFileName())
-		
+        
     if args.mails:
         SearchEmails(GetOutFileName())
-		
+        
     if args.urls:
         SearchUrls(GetOutFileName())
-		
+        
     if args.fb:
-        SearchFBChat(GetOutFileName())	
+        SearchFBChat(GetOutFileName())    
 
     if args.lang:
-        SearchLanguage(GetOutFileName(), args.lang)
-	
+        SearchLanguage(GetOutFileName(), args.lang[0])
+    
     print("\n")
     os.remove(GetOutFileName())
